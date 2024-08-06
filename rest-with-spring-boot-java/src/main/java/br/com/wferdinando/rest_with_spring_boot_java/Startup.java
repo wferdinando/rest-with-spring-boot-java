585405933f081efb054afdfb8f4cf138d7d2cf7c
Willyan Trautwein Ferdinando <willyantferdinando@gmail.com>
2024-08-06 08:30:42 -0300

    Projeto Inicial

---
+++ rest-with-spring-boot-java/src/main/java/br/com/wferdinando/rest_with_spring_boot_java/Startup.java
@@ -0,0 +1,13 @@
+package br.com.wferdinando.rest_with_spring_boot_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Startup {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Startup.class, args);
+	}
+
+}
---
+++ rest-with-spring-boot-java/src/test/java/br/com/wferdinando/rest_with_spring_boot_java/StartupTests.java
@@ -0,0 +1,13 @@
+package br.com.wferdinando.rest_with_spring_boot_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StartupTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
